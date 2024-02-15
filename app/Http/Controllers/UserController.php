7f57499b2594af4cbf753158d29202c6a65f28e7
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-02-16 00:54:41 +0700

    fix: peminjaman

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mobil;
+use App\Models\Pengembalian;
 use App\Models\Sewa;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
@@ -16,7 +18,7 @@ class UserController extends Controller
     public function index(): View
     {
         $mobils = Mobil::all();
-        return view('dashboard', compact('mobils'));
+        return view('user.dashboard', compact('mobils'));
     }
 
     /**
@@ -32,45 +34,64 @@ class UserController extends Controller
      */
     public function sewa(Request $request)
     {
+        if ($request->status == 'disewa') {
+            return redirect()->back();
+        }
         $request->validate([
             'tanggal_mulai' => 'required',
             'tanggal_selesai' => 'required',
         ]);
 
+        $tglAwal = $request->tanggal_mulai;
+        $tglAkhir = $request->tanggal_selesai;
+        $startCarbon = Carbon::parse($tglAwal);
+        $endCarbon = Carbon::parse($tglAkhir);
+        $harga_sewa = $request->tarif_sewa * ($endCarbon->diffInDays($startCarbon) + 1);
 
         $sewa = new Sewa();
         $sewa->id_user = Auth::user()->id;
         $sewa->tanggal_mulai = $request->tanggal_mulai;
         $sewa->tanggal_selesai = $request->tanggal_selesai;
         $sewa->id_mobil = $request->id_mobil;
-        $sewa->harga_sewa = $request->tarif_sewa;
+        $sewa->harga_sewa = $harga_sewa;
         $sewa->save();
+        Mobil::where('id', $request->id_mobil)->update(['status' => 'disewa']);
 
-        return redirect()->route('user.homepage');
+        return redirect()->back();
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function getHargaSewa(Request $request)
     {
-        //
+        $tglAwal = $request->tanggal_mulai;
+        $tglAkhir = $request->tanggal_selesai;
+        $startCarbon = Carbon::parse($tglAwal);
+        $endCarbon = Carbon::parse($tglAkhir);
+
+        return $endCarbon->diffInDays($request->tarif_sewa * $startCarbon);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function sewaBerlangsung()
     {
-        //
+        $sewas = Sewa::where('id_user', Auth::user()->id)->join('mobils', 'sewas.id_mobil', '=', 'mobils.id')->select('sewas.*', 'mobils.merek', 'mobils.plat', 'mobils.status')->get();
+        // @dd($sewas);
+        return view('user.riwayat', compact('sewas'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+
+    public function pengembalian(Request $request)
     {
-        //
+        $sewas = Sewa::where('id_user', Auth::user()->id)->join('mobils', 'sewas.id_mobil', '=', 'mobils.id')->select('sewas.*', 'mobils.plat', 'mobils.status')->get();
+        $checkPlat = $sewas->where('plat', $request->plat);
+
+
+        if (count($checkPlat) >= 1) {
+            @dd($checkPlat->update(['status' => 'tersedia']));
+            $checkPlat->update(['status' => 'tersedia']);
+            Pengembalian::created(['id_sewa' => $checkPlat->id]);
+            return redirect()->back();
+        } else {
+            return redirect()->back();
+        }
     }
 
     /**
